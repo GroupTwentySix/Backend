4c7787e0467bbf3d05b4702cb8db27632db279d6
axolotl32 <167874994+axolotl32@users.noreply.github.com>
2024-10-16 10:05:19 +0100

    Update Main.java

--- src/main/java/cc/grouptwentysix/vitality/Main.java
+++ src/main/java/cc/grouptwentysix/vitality/Main.java
@@ -1,15 +1,1 @@
-package cc.grouptwentysix.vitality;
 
-import io.javalin.Javalin;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("[DEBUG] App Start, Hello World");
-        var app = Javalin.create(/*config*/)
-                .get("/", ctx -> ctx.result("Hello World"))
-                .start(7071);
-        System.out.println("[DEBUG] Route / started at http://localhost:7070/");
-    }
-}
