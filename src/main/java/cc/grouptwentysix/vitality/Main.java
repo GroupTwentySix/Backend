6f33b847e015d663bc23e811d61c49f28821dd7f
Jake Squelch <jakewsquelch@gmail.com>
2025-03-20 21:33:37 +0000

    test

--- src/main/java/cc/grouptwentysix/vitality/Main.java
+++ src/main/java/cc/grouptwentysix/vitality/Main.java
@@ -15,7 +15,6 @@ public class Main {
 
     public static Dotenv dotenv;
 
-
     public static void main(String[] args) {
 
         dotenv = Dotenv.configure().ignoreIfMissing().load();
@@ -70,6 +69,4 @@ public class Main {
 
 
     }
-
-
 }
